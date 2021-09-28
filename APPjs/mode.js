4b6618de651693607aecaf8e75cc26f020016cd7
ricardoggz <ricardo_ggzicm@outlook.com>
2021-09-28 16:57:17 -0500

    se almacena en el evento dark mode en localStorage

--- APPjs/mode.js
+++ APPjs/mode.js
@@ -1,6 +1,25 @@
+//Cambio a modo obscuro
 const mode = document.querySelector("#btn-mode");
 const body = document.querySelector("body");
 
+savePage();
+
 mode .addEventListener("click", e => {
     body.classList.toggle("mode");
-})
+    refreshPage(body.classList.contains('mode'));
+});
+
+//almacenar el evento en localstorage
+
+function savePage(){
+    const mode = localStorage.getItem('mode');
+    if(!mode){
+        refreshPage('false');
+    }else if(mode == 'true'){
+        body.classList.add('mode');
+    }
+}
+
+function refreshPage(value){
+    localStorage.setItem('mode', value);
+}
